03eb7a1e2eb7601247f5f6c4b64f5b594b0c5868
Lluis Peinado <lpeinado@stelladot.com>
2023-05-13 20:25:41 +0200

    chore(init): yarn workspace structure linking 1 vue project

---
+++ lib/src/widget.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="widget">
+      {{ msg  }} TRALARA
+  </div>
+</template>
+
+<script>
+  export default {
+      name: 'widget',
+      props: {
+          msg: String
+      }
+  }
+</script>
